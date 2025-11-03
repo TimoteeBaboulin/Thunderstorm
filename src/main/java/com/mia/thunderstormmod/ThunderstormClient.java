bb52dad25d4323595f8de455e86d8de61718531b
theodu30_ <121396182+theodu30@users.noreply.github.com>
2025-11-03 19:25:29 +0100

    Clean up main java class

--- src/main/java/com/mia/thunderstormmod/ThunderstormClient.java
+++ src/main/java/com/mia/thunderstormmod/ThunderstormClient.java
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = Thunderstorm.MODID, dist = Dist.CLIENT)
+@Mod(value = Thunderstorm.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = Thunderstorm.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = Thunderstorm.MOD_ID, value = Dist.CLIENT)
 public class ThunderstormClient {
     public ThunderstormClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
